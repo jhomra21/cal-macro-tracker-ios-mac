e176ad5a95ca233fcdebc00eb7929ac17a0a2dc6
juan <jhonra@icloud.com>
2026-04-01 12:01:37 -0500

    Initial Commit

---
+++ cal-macro-tracker/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  cal-macro-tracker
+//
+//  Created by Juan Martinez on 4/1/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ cal-macro-tracker/cal_macro_trackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  cal_macro_trackerApp.swift
+//  cal-macro-tracker
+//
+//  Created by Juan Martinez on 4/1/26.
+//
+
+import SwiftUI
+
+@main
+struct cal_macro_trackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
